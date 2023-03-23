1ddea19b2cd5be31429b4422ff06b93e95c60086
Kenta Shimosawa <shimon101018@gmail.com>
2023-03-23 17:38:14 +0900

    feat: 🎸 cctx public examples

---
+++ m23mm.py
@@ -0,0 +1,139 @@
+import ccxt
+from pprint import pprint
+
+bitflyer = ccxt.bitflyer()
+
+symbol = "BTC/JPY"
+
+def get_market():
+# ----------------------------------------------
+# 取引所の通貨情報を取得。例：
+# 'BTC/JPY': {'active': True,
+            #  'base': 'BTC',
+            #  'baseId': 'BTC',
+            #  'contract': False,
+            #  'contractSize': None,
+            #  'expiry': None,
+            #  'expiryDatetime': None,
+            #  'future': False,
+            #  'id': 'BTC_JPY',
+            #  'info': {'market_type': 'Spot', 'product_code': 'BTC_JPY'},
+            #  'inverse': None,
+            #  'limits': {'amount': {'max': None, 'min': None},
+            #             'cost': {'max': None, 'min': None},
+            #             'leverage': {'max': None, 'min': None},
+            #             'price': {'max': None, 'min': None}},
+            #  'linear': None,
+            #  'maker': 0.002,
+            #  'margin': False,
+            #  'option': False,
+            #  'optionType': None,
+            #  'percentage': True,
+            #  'precision': {'amount': None, 'price': None},
+            #  'quote': 'JPY',
+            #  'quoteId': 'JPY',
+            #  'settle': None,
+            #  'settleId': None,
+            #  'spot': True,
+            #  'strike': None,
+            #  'swap': False,
+            #  'symbol': 'BTC/JPY',
+            #  'taker': 0.002,
+            #  'type': 'spot'},
+  markets = bitflyer.load_markets()
+  pprint(markets)
+
+# ------------------------------------------------------------
+# 取引所の板情報を取得
+#  'ask': 3649058.0,
+#  'askVolume': None,
+#  'average': None,
+#  'baseVolume': 3499.57269325,
+#  'bid': 3647991.0,
+#  'bidVolume': None,
+#  'change': None,
+#  'close': 3648140.0,
+#  'datetime': '2023-03-23T08:31:22.247Z',
+#  'high': None,
+#  'info': {'best_ask': '3649058.0',
+#           'best_ask_size': '0.02',
+#           'best_bid': '3647991.0',
+#           'best_bid_size': '0.00911',
+#           'ltp': '3648140.0',
+#           'market_ask_size': '0.0',
+#           'market_bid_size': '0.0',
+#           'product_code': 'BTC_JPY',
+#           'state': 'RUNNING',
+#           'tick_id': '7534812',
+#           'timestamp': '2023-03-23T08:31:22.247',
+#           'total_ask_depth': '309.12035872',
+#           'total_bid_depth': '498.59724936',
+#           'volume': '14089.71756056',
+#           'volume_by_product': '3499.57269325'},
+#  'last': 3648140.0,
+#  'low': None,
+#  'open': None,
+#  'percentage': None,
+#  'previousClose': None,
+#  'quoteVolume': None,
+#  'symbol': 'BTC/JPY',
+#  'timestamp': 1679560282247,
+#  'vwap': None}
+def fetch_ticker(symbol):
+  ticker = bitflyer.fetch_ticker(symbol)
+  pprint(ticker)
+
+# ---------------------------------------------
+# 板情報を注文量と合わせて取得
+#           .....
+#           [1568800.0, 3.0],
+#           [1561350.0, 0.001],
+#           [1551000.0, 0.06],
+#           [1550000.0, 0.517],
+#           [1535000.0, 0.1],
+#           [1530000.0, 0.1],
+#           [1528000.0, 0.1],
+#           [1524650.0, 0.003],
+#           [1519400.0, 0.001],
+#           [1506300.0, 0.006],
+#           [1500000.0, 3.15933866],
+#           [1420000.0, 0.5],
+#           [1400000.0, 0.084],
+#           [1399000.0, 0.00104999],
+#           [1380000.0, 1.4655],
+#           [1375000.0, 1.0],
+#           [1370000.0, 1.0],
+#           [61895.0, 0.00849]],
+#  'datetime': None,
+#  'nonce': None,
+#  'symbol': 'BTC/JPY',
+#  'timestamp': None}
+def fetch_order_book(symbol="BTC/JPY"):
+  order_book = bitflyer.fetch_order_book(symbol)
+  pprint(order_book)
+
+
+# ---------------------------------------------
+# ローソク足情報の取得
+# [[1679560440000,
+#   3645618.0,
+#   3645618.0,
+#   3644354.0,
+#   3644354.0,
+#   0.14823398000000002],
+#  [1679560500000,
+#   3644354.0,
+#   3644651.0,
+#   3642795.0,
+#   3643440.0,
+#   1.4045539000000002],
+#  [1679560560000, 3643440.0, 3643440.0, 3643438.0, 3643438.0, 0.04230954]]
+def fetch_candles(timeframe='1m', symbol='BTC/JPY'):
+  candles = bitflyer.fetch_ohlcv(symbol, timeframe, since=None, limit=None, params={})
+  pprint(candles)
+
+if __name__ == "__main__":
+  # get_market()
+  # fetch_ticker(symbol)
+  # fetch_order_book(symbol)
+  fetch_candles()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import ccxt
 import datetime
+import time
+import json
 
 # TODO:
 # 1. o 現在の価格を取得
@@ -17,8 +19,14 @@ import datetime
 # 取引ペア
 symbol = "BTC/JPY"
 
+api_key = "todo"
+api_secret = "todo"
+
 # BitFlyerのAPIにアクセスするために必要な認証情報が設定
-exchange = ccxt.bitflyer()
+exchange = ccxt.bitflyer({
+  api_key: api_key,
+  api_secret: api_secret
+})
 
 # 時刻系
 date_format = "%Y年%m月%d日 %H時%M分%S秒"
@@ -31,11 +39,95 @@ def get_ticker():
     last_price = ticker["last"]
     return last_price
 
-def main():
+def place_orders():
+   try:
+    current_price = get_ticker()
+
+    # 買い注文と売り注文の価格を計算
+    bid_price = current_price * 0.995
+    ask_price = current_price * 1.005
+
+    # 注文量を指定
+    order_amount = 0.001
+
+    # 買い注文を出す
+    bid_order = exchange.create_limit_buy_order(symbol, order_amount, bid_price)
+    print(f"Buy order placed at {buy_price}")
+
+    # 売り注文を出す
+    ask_order = exchange.create_limit_sell_order(symbol, order_amount, ask_price)
+    print(f"Sell order placed at {ask_price}")
+   except Exception as e:
+    print("f:Error occurred: {e}")
+
+
+# 過去のデータを取得
+def fetch_historical_data():
+  timeframe = "1h"
+  since = exchange.parse8601('2022-01-01T00:00:00Z')
+  now = exchange.milliseconds()
+
+  all_candles = []
+  while since < now:
+    candles = exchange.fetch_ohlcv(symbol, timeframe, since)
+    if len(candles)==0:
+      break
+    since = candles[-1][0]+1
+    print(f"since: {since} candles: {candles}")
+    all_candles += candles
+
+  file = open("./{0}-{1}-price.json".format(since, now))
+  json.dump(all_candles, file)
+  return all_candles
+
+def back_test(data):
+  print(f"data: {data}")
+  # 開始時のバランス
+  balance = 100000
+  order_amount = 0.001
+
+  for i in range(len(data)-1):
+    current_price = data[i][4] # 終値
+    next_open_price = data[i+1][1] #次の始値
+    print(f"current_price: {current_price}, next_open_price: {next_open_price}")
+
+    bid_price = current_price * 0.995
+    ask_price = current_price * 1.005
+
+    print(f"bid: {bid_price}, next_open_price: {next_open_price}, next_open_price <= bid_price: {next_open_price<=bid_price}")
+
+    if next_open_price <= bid_price:
+      balance -= order_amount * bid_price
+      balance += order_amount * ask_price
+
+    # buy_price = current_price * 1.005
+    # sell_price = current_price * 0.995
+
+    # if buy_price <= next_open_price <= sell_price:
+    #   balance -= order_amount * buy_price
+    #   balance += order_amount * sell_price
+
+  return balance
+
+def test():
+  historical_data = fetch_historical_data()
+  initial_balance = 100000
+  final_balance = back_test(historical_data)
+  profit = final_balance - initial_balance
+  print(f"Initial balance: {initial_balance}")
+  print(f"Final balance: {final_balance}")
+  print(f"Profit: {profit}")
+
+def fetch_realtime_data():
   while True:
     last_price = get_ticker()
     now = datetime.datetime.now()
     print(f"Bitflyer {symbol}: ", last_price, f"{now.strftime(date_format)}")
+    time.sleep(3)
+  
+
+def main():
+  test()
 
 if __name__ == "__main__":
     main()
